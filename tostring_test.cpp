5682731ca694c859eded51b0f70df98d02165869
m-tovar <marceldavidtovar@hotmail.com>
2017-02-12 19:14:36 +0100

    added string_cast<T> to tostringtest.cpp -- works only for standard types

--- tostring_test.cpp
+++ tostring_test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -10,12 +11,31 @@ string toString(T& obj){
 	return converter.str();
 }
 
+template <typename T>
+T string_cast(const string& in_str){
+	istringstream converter;
+	T ret_val;
+	converter.str(in_str);
+	if (converter >> ret_val){
+		return ret_val;
+	}
+	throw std::invalid_argument("Invalid argument");
+}
+
+
+
 int main() {
 	int a = 44;
 	cout << toString(a) << endl;
 
 	double b = 1337.7331;
 	cout << toString(b) << endl;
+
+	double c = string_cast<double>("14.04");
+	cout << toString(c) << endl;
+
+	double d = string_cast<double>("wrongstring");
+	cout << toString(d) << endl;
 
 	return 0;
 }
